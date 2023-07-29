176a93b512e9aa1721fafd91a602a38267ebfd37
yongruifang <1102649231@qq.com>
2023-07-29 20:11:56 +0800

    Test: 循环插入十条语句

--- src/test/java/com/example/test/TestHibernate.java
+++ src/test/java/com/example/test/TestHibernate.java
@@ -21,5 +21,20 @@ public class TestHibernate {
         s.close();
         sf.close();
     }
- 
+    //循环插入10个Product对象到数据库中
+    @Test
+    public void insert10Products(){
+        SessionFactory sf = new Configuration().configure().buildSessionFactory();
+        Session s = sf.openSession();
+        s.beginTransaction();
+        for(int i = 0 ; i < 10 ; i ++){
+            Product p  = new Product();
+            p.setName("iphone"+i);
+            p.setPrice(5000+i);
+            s.persist(p);
+        }
+        s.getTransaction().commit();
+        s.close();
+        sf.close();
+    }
 }
